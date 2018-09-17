92c80af2c17ff094dd161137c9ea229a5eed9f84
Tyler Brothers <tylerbrothers1@gmail.com>
2018-09-16 21:57:48 -0600

    Bugfix: Deck's init working, mismatched property and init variable names.

--- Deck.py
+++ Deck.py
@@ -3,8 +3,8 @@ from CardSets.TierOne import TierOne
 from CardSets.TierTwo import TierTwo
 
 class Deck(object):
-  def __init__(self, card=[]):
-      self.cards = cards
+  def __init__(self, cards=[]):
+    self.cards = cards
 
   def new(num_players):
     return Deck(_new_game_cards(num_players))
